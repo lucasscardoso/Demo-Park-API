a60f05ead59f59dd2722f7fa9041af30e3fed5dd
lucasscardoso <gajeel.red.fox@hotmail.com>
2025-11-18 08:44:12 -0300

    criado tratamento de exceptions

---
+++ src/main/java/com/curso/demo_park_api/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.curso.demo_park_api.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request, BindingResult result){
+
+        logger.error("API ERROR - "+ ex);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).
+                contentType(MediaType.APPLICATION_JSON).
+                body(new ErrorMessage(request,HttpStatus.UNPROCESSABLE_ENTITY,"Campo(s) invalidos!",result));
+    }
+}
